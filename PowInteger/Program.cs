e53d7e05f79f6678405e452b7f2f63c49743bda3
EduardooOrtega <openingthedoor@yandex.ru>
2022-02-25 12:13:00 +0300

    Add PowInteger

---
+++ PowInteger/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+double RaisingRecBit(double x, double degree)
+{
+    if (degree == 0)
+    {
+        return 1;
+    }
+    if (degree % 2 == 1)
+    {
+        return RaisingRecBit(x, degree - 1) * x;
+    }
+    else
+    {
+        return RaisingRecBit(x, degree / 2) * RaisingRecBit(x, degree / 2);
+    }
+}
+
+
+
+string s = " ";
+while (s != "")
+{
+    Console.Write("x = ");
+    double x = double.Parse(Console.ReadLine());
+    Console.Write("степень = ");
+    double degree = double.Parse(Console.ReadLine());
+
+    if (degree >= 0)
+    {
+        Console.WriteLine(RaisingRecBit(x, degree));
+    }
+    else
+    {
+        degree = degree * (-1);
+        Console.WriteLine(1 / RaisingRecBit(x, degree));
+    }
+
+    Console.Write("для выхода нажмите enter");
+    s = Console.ReadLine();
+}
+
+
